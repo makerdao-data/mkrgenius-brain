f8d137fb3c331d2d5b5bb00176f826c1977e0aa4
Tadeo <tadeo@Tadeos-Air.fibertel.com.ar>
2023-04-13 18:51:06 -0300

    fixed deploy

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ set -e
 
 MKRGENIUS_HOME=${1:-$HOME/mkrgenius-brain}
 cd $MKRGENIUS_HOME
-docker pull ghcr.io/tadeongmi/mkrgenius-brain
+docker pull ghcr.io/makerdao-data/mkrgenius-brain
 docker-compose down
 docker-compose up -d
