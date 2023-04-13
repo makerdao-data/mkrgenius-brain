e406c3a3fc09f8ffc079ee51b1895de9066cf577
Tadeo <tadeo@Tadeos-Air.fibertel.com.ar>
2023-04-13 18:33:26 -0300

    deployment changes

--- update_index.sh
+++ update_index.sh
@@ -5,7 +5,7 @@ MKRGENIUS_HOME=${1:-$HOME/mkrgenius-brain}
 echo "updating training-data index..."
 cd $MKRGENIUS_HOME
 docker rm -f mkrgenius-builder || true
-docker run --env-file=.env --name mkrgenius-builder --entrypoint "/app/build_index.sh" ghcr.io/tadeongmi/mkrgenius-brain
+docker run --env-file=.env --name mkrgenius-builder --entrypoint "/app/build_index.sh" ghcr.io/makerdao-data/mkrgenius-brain
 docker cp mkrgenius-builder:/app/index_new.json .
 docker rm -f mkrgenius-builder || true
 docker-compose down
