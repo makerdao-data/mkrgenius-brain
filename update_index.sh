9c8e4e7feda450f2e756c7dde0175bbe441ab48a
pk <piotr.m.klis@gmail.com>
2023-04-20 16:33:39 +0200

    added build command to update index proc

--- update_index.sh
+++ update_index.sh
@@ -7,6 +7,7 @@ cd $MKRGENIUS_HOME
 # stopping and removing the container as we're creating a new one
 sudo docker stop mkrgenius-builder || true
 sudo docker rm -f mkrgenius-builder || true
+sudo docker build . -t brain:builder
 echo "starting the container and creating fresh index..."
 sudo docker run --env-file=.env --name mkrgenius-builder --entrypoint "/app/build_index.sh" brain:builder
 sudo docker cp mkrgenius-builder:/app/index_new.json .
@@ -19,4 +20,4 @@ sudo docker stop dev-mkrgenius-brain
 sudo docker run -d -v $MKRGENIUS_HOME/index.json:/app/index.json --name dev-mkrgenius-brain --env-file .env -p 8000:8000 brain:dev
 echo "deleting image used for index update..."
 docker rmi -f brain:builder
-docker system -f prune
+docker system prune -f
